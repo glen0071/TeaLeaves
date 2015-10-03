05b572f223b2cd39c09c2c6aa4e8f112bb8e718c
Daniel Greuel <dgreuel@gmail.com>
2015-10-02 21:27:14 -0500

    added questionDetail route

---
+++ client/interface/questions/view questions/questionDetail.js
@@ -0,0 +1,21 @@
+Template.questionDetail.helpers({
+  questions: function(){
+    return Questions.find();
+  },
+  userName:function(){
+    var user=this.createdBy;
+    return Meteor.users.findOne({_id:user}).emails[0].address;
+  },
+  createdOnString:function(){
+    var created=this.createdOn;
+    return moment(created).format("dddd, MMMM D, YYYY, h:mm a");
+  },
+  deadlineString:function(){
+    var dead=this.deadline;
+    return moment(dead).format("dddd, MMMM D, YYYY, h:mm a");
+  }
+});
+
+Template.questionDetail.onCreated(function () {
+    this.subscribe('questions');
+});
