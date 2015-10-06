52e86cea577ed57a85309dca7c511cdfe7b17b7c
Eddie Glenn <Eddie Glenn>
2015-10-06 16:00:16 -0500

    profile page includes user created questions

--- client/interface/accounts/myProfile.js
+++ client/interface/accounts/myProfile.js
@@ -1,9 +1,23 @@
 // A user can view and edit his profile from here
+Template.myProfile.onCreated(function () {
+    this.subscribe('questions');
+    // limit to questions user created or answered
+});
+
 
 Template.myProfile.helpers({
-  // rendered: function(){
-  //
-  // }
+  questionsAsked: function(){
+    var currentUser = Meteor.userId();
+    return Questions.find({createdBy: currentUser}).count();
+  },
+  questionsUserAsked: function(){
+    var currentUser = Meteor.userId();
+    return Questions.find({createdBy: currentUser});
+  },
+  questionsUserAnswered: function(){
+    var currentUser = Meteor.userId();
+    return Questions.find({answeredBy: currentUser});
+  }
 });
 
 Template.myProfile.events({
