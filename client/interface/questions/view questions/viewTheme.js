06c30e4ffe11a244c9a01fb20160fb3ed1969605
Daniel Greuel <dgreuel@gmail.com>
2015-10-05 23:05:07 -0500

    added viewTheme route

---
+++ client/interface/questions/view questions/viewTheme.js
@@ -0,0 +1,14 @@
+Template.viewTheme.onCreated(function () {
+    this.subscribe('questions');
+});
+
+Template.viewTheme.helpers({
+  theme:function(){
+    return Iron.controller().getParams().theme;
+  },
+  questions: function(){
+    var theTheme = Iron.controller().getParams().theme;
+    //console.log('theTheme: '+theTheme);
+    return Questions.find({themes:theTheme}, {sort: {createdOn: -1}});
+  }
+});
