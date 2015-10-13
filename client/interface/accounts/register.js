3203c30eb668ec48bfa6c2849e7871c48e432b9c
Daniel Greuel <dgreuel@gmail.com>
2015-10-13 09:07:20 -0500

    login immediately after registration

--- client/interface/accounts/register.js
+++ client/interface/accounts/register.js
@@ -11,5 +11,7 @@ Template.register.events({
       followingThemes: [],
       followingUsers: []
     });
+    Meteor.loginWithPassword(varEmail, varPassword);
+    Router.go("viewQuestions");
   }
 });
