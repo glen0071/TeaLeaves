b5b8b4b584bcf82af4c60d4687db3ab980cbe1be
Daniel Greuel <dgreuel@gmail.com>
2015-10-23 22:48:03 -0500

    added infinite scrolling via pages package

--- client/interface/questions/view questions/viewQuestions.js
+++ client/interface/questions/view questions/viewQuestions.js
@@ -1,50 +1,9 @@
 Template.viewQuestions.onCreated(function () {
-    this.newQs = new ReactiveVar( true );
-    this.oldQs = new ReactiveVar( false );
-    this.endQs = new ReactiveVar( false );
-    this.popQs = new ReactiveVar( false );
-    this.unpopQs = new ReactiveVar( false );
-    this.topRatedQs = new ReactiveVar( false );
 
 });
 
 Template.viewQuestions.helpers({
-  newQs: function(){
-    return Template.instance().newQs.get();
-  },
-  showNewQs: function(){
-    return Questions.find({}, {sort: {createdOn: -1}});
-  },
-  oldQs: function(){
-    return Template.instance().oldQs.get();
-  },
-  showOldQs: function(){
-    return Questions.find({}, {sort: {createdOn: 1}});
-  },
-  endQs: function(){
-    return Template.instance().endQs.get();
-  },
-  showEndQs: function(){
-    return Questions.find({}, {sort: {deadline: 1}});
-  },
-  popQs: function(){
-    return Template.instance().popQs.get();
-  },
-  showPopQs: function(){
-    return Questions.find({}, {sort: {answerCount: -1}})
-  },
-  unpopQs: function(){
-    return Template.instance().unpopQs.get();
-  },
-  showUnpopQs: function(){
-    return Questions.find({}, {sort: {answerCount: 1}})
-  },
-  topRatedQs: function(){
-    return Template.instance().topRatedQs.get();
-  },
-  showTopRatedQs: function(){
-    return Questions.find({}, {sort: {upVotes: -1}})
-  },
+
 });
 
 Template.viewQuestions.events({
@@ -52,54 +11,35 @@ Template.viewQuestions.events({
     var btnValue = $('[name=viewQsBtn]:checked').val();
       switch (btnValue) {
         case "1":
-          template.newQs.set(true);
-          template.oldQs.set(false);
-          template.endQs.set(false);
-          template.popQs.set(false);
-          template.unpopQs.set(false);
-          template.topRatedQs.set(false);
+          Pages.set({
+    sort: {createdOn: -1}
+  });
           break;
         case "2":
-          template.oldQs.set(true);
-          template.newQs.set(false);
-          template.endQs.set(false);
-          template.popQs.set(false);
-          template.unpopQs.set(false);
-          template.topRatedQs.set(false);
+          Pages.set({
+    sort: {createdOn: 1}
+  });
           break;
         case "3":
-          template.endQs.set(true);
-          template.oldQs.set(false);
-          template.newQs.set(false);
-          template.popQs.set(false);
-          template.unpopQs.set(false);
-          template.topRatedQs.set(false);
+          Pages.set({
+    sort: {deadline: 1}
+  });
           break;
         case "4":
-          template.popQs.set(true);
-          template.endQs.set(false);
-          template.oldQs.set(false);
-          template.newQs.set(false);
-          template.unpopQs.set(false);
-          template.topRatedQs.set(false);
+          Pages.set({
+    sort: {answerCount: -1}
+  });
           break;
         case "5":
-          template.unpopQs.set(true);
-          template.popQs.set(false);
-          template.endQs.set(false);
-          template.oldQs.set(false);
-          template.newQs.set(false);
-          template.topRatedQs.set(false);
+          Pages.set({
+    sort: {answerCount: 1}
+  });
           break;
         case "6":
-          template.topRatedQs.set(true);
-          template.unpopQs.set(false);
-          template.popQs.set(false);
-          template.endQs.set(false);
-          template.oldQs.set(false);
-          template.newQs.set(false);
+          Pages.set({
+    sort: {upVotes: -1}
+  });
           break;
-        default: template.newQs.set(true);
       }
   },
 });
