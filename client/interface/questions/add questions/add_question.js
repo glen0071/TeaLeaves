4e96da010000101c6568a04ae07594e6b1b76606
Daniel Greuel <dgreuel@gmail.com>
2015-10-26 12:14:47 -0500

    don't allow question headline to be left blank

--- client/interface/questions/add questions/add_question.js
+++ client/interface/questions/add questions/add_question.js
@@ -1,23 +1,30 @@
+Template.addQuestion.onRendered(function() {
+var validator = $('.add-question-form').validate({
+    submitHandler: function(event){
+      var varHeadline = $('[name=headline]').val();
+      var varText = $('[name=text]').val();
+      var varThemes = $('[name=themes]').val().split(/,+\s*/);
+      var varDeadline=$('.datetimepicker').data("DateTimePicker").date().toDate();
+      Meteor.call('createNewQuestion', varHeadline, varText, varThemes, varDeadline, function(error,results) {
+        if(error){
+          console.log(error.reason);
+        }else{
+          Router.go('questionDetail', {
+            _id: results
+          });
+          $('[name=headline]').val('');
+          $('[name=themes]').val('');
+          $('[name=text]').val('');
+          $('.datetimepicker').val('');
+        }
+      });
+    }
+});
+});
+
 Template.addQuestion.events({
   'submit form': function(event, template){
     event.preventDefault();
-    var varHeadline = $('[name=headline]').val();
-    var varText = $('[name=text]').val();
-    var varThemes = $('[name=themes]').val().split(/,+\s*/);
-    var varDeadline=$('.datetimepicker').data("DateTimePicker").date().toDate();
-    Meteor.call('createNewQuestion', varHeadline, varText, varThemes, varDeadline, function(error,results) {
-      if(error){
-        console.log(error.reason);
-      }else{
-        Router.go('questionDetail', {
-          _id: results
-        });
-        $('[name=headline]').val('');
-        $('[name=themes]').val('');
-        $('[name=text]').val('');
-        $('.datetimepicker').val('');
-      }
-    });
   }
 });
 Template.addQuestion.onRendered(function() {
