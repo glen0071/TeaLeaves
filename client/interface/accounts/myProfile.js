9558355f1742087cee4323de043c8875efcc31dd
glen0071 <glen0071@gmail.com>
2015-12-06 18:01:19 -0600

    user can change password in my profile

--- client/interface/accounts/myProfile.js
+++ client/interface/accounts/myProfile.js
@@ -1,27 +1,38 @@
 // A user can view and edit his profile from here
-Template.myProfile.onCreated(function () {
-    this.subscribe('questions', {limit: 100});
-    this.editProfileMode = new ReactiveVar( false );
+Template.myProfile.onCreated(function() {
+  this.subscribe('questions', {
+    limit: 100
+  });
+  this.editProfileMode = new ReactiveVar(false);
+  this.editingPassword = new ReactiveVar(false);
 });
 
 Template.myProfile.helpers({
-  questionsAsked: function(){
+  questionsAsked: function() {
     var currentUser = Meteor.userId();
-    return Questions.find({createdBy: currentUser}).count();
+    return Questions.find({
+      createdBy: currentUser
+    }).count();
   },
-  questionsUserAsked: function(){
+  questionsUserAsked: function() {
     var currentUser = Meteor.userId();
-    return Questions.find({createdBy: currentUser});
+    return Questions.find({
+      createdBy: currentUser
+    });
   },
-  questionsAnswered: function(){
+  questionsAnswered: function() {
     var currentUser = Meteor.userId();
-    return Questions.find({"answers.userId": currentUser}).count();
+    return Questions.find({
+      "answers.userId": currentUser
+    }).count();
   },
-  questionsUserAnswered: function(){
+  questionsUserAnswered: function() {
     var currentUser = Meteor.userId();
-    return Questions.find({"answers.userId": currentUser});
+    return Questions.find({
+      "answers.userId": currentUser
+    });
   },
-  userName: function(){
+  userName: function() {
     return Meteor.user().username ? Meteor.user().username : '';
   },
   aboutMe: function() {
@@ -39,32 +50,79 @@ Template.myProfile.helpers({
   usersFollowed: function() {
     return Meteor.user().profile.followingUsers.join(', ');
   },
+  editingPassword: function() {
+    return Template.instance().editingPassword.get();
+  }
 });
 
 Template.myProfile.events({
-  'click #edit-profile-link': function(event, template){
-      event.preventDefault();
-      template.editProfileMode.set( true );
+  'click #edit-profile-link': function(event, template) {
+    event.preventDefault();
+    template.editProfileMode.set(true);
   },
-  'submit form': function(event, template){
-      event.preventDefault();
-      var newUserName = $('[name=username]').val();
-      var newAboutMe = $('[name=aboutme]').val();
-      var newName = $('[name=name]').val();
-      Meteor.call('updateProfile', newUserName, newAboutMe, newName)
-      template.editProfileMode.set( false );
+  'submit form': function(event, template) {
+    event.preventDefault();
+    var newUserName = $('[name=username]').val();
+    var newAboutMe = $('[name=aboutme]').val();
+    var newName = $('[name=name]').val();
+    Meteor.call('updateProfile', newUserName, newAboutMe, newName)
+    template.editProfileMode.set(false);
   },
-  'click #cancel-changes': function(event, template){
-    template.editProfileMode.set( false );
+  'click #cancel-changes': function(event, template) {
+    template.editProfileMode.set(false);
   },
-  'click #delete-account': function(event, template){
+  'click #delete-account': function(event, template) {
     event.preventDefault();
     console.log("test");
     var dont_go = confirm("Are you sure you'd like to delete your account?");
-    if(dont_go){
+    if (dont_go) {
       var currentUser = Meteor.userId();
       Meteor.call('delete_current_user', currentUser)
-      // automatically goes to viewQuestions page already
+        // automatically goes to viewQuestions page already
     }
-  }
+  },
+  'click #add-email': function(event,template){
+    event.preventDefault();
+    console.log("message");
+
+  },
+  'click #edit-password-module': function(event,template){
+    event.preventDefault();
+    console.log("edit password");
+    template.editingPassword.set(true);
+  },
+  'click #update-password': function(event,template){
+    event.preventDefault();
+    var oldPassword = $('[name=currentPassword]').val();
+    var newPassword = $('[name=confirmPassword]').val();
+    var confirmPassword = $('[name=confirmPassword]').val();
+    console.log(oldPassword + "  " + newPassword + " " + confirmPassword);
+    if (newPassword !== confirmPassword) {
+            alert("passwords don't match")
+            return false;
+        }
+    Accounts.changePassword(oldPassword, newPassword, function(error) {
+            if (error) {
+                console.log('There was an issue: ' + error.reason);
+            } else {
+                console.log('You reset your password!');
+            }
+    });
+  },
+  'click #cancel-change': function(event,template){
+    event.preventDefault();
+    console.log("cancel save password");
+    template.editingPassword.set(false);
+  },
+  //   Meteor.call("userAddEmail", dataObject, function(error, result){
+  //     if(error){
+  //       console.log("error", error);
+  //     }
+  //     if(result){
+  //
+  //     }
+  //   });
+  //
+
+
 });
