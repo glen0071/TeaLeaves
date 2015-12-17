83d3ac2f016502fce3bd48eed45980ccf54a399b
Daniel Greuel <dgreuel@gmail.com>
2015-12-17 12:11:07 -0600

    replace voting buttons with reactive switch component

--- client/interface/questions/view or edit question/voting.js
+++ client/interface/questions/view or edit question/voting.js
@@ -1,186 +1,185 @@
 Template.voting.helpers({
-  unanswered:function(){
+  id: function() {
+    return this._id;
+  },
+  unanswered: function() {
     var answersArray = this.answers;
     var currentUser = Meteor.userId();
-    var filtered = answersArray.filter(function(object){
+    var filtered = answersArray.filter(function(object) {
       return (object.userId === currentUser)
     });
-    if(filtered.length === 0) {
+    if (filtered.length === 0) {
       return true
     }
   },
-  answered:function(){
+  answered: function() {
     var answersArray = this.answers;
     var currentUser = Meteor.userId();
-    var filtered = answersArray.filter(function(object){
+    var filtered = answersArray.filter(function(object) {
       return (object.userId === currentUser)
     });
-    if(filtered.length > 0) {
+    if (filtered.length > 0) {
       return true
     }
   },
-  answeredYes:function(){
+  answeredYes: function() {
     var answersArray = this.answers;
     var currentUser = Meteor.userId();
-    var filtered = answersArray.filter(function(object){
+    var filtered = answersArray.filter(function(object) {
       return ((object.userId === currentUser) && (object.answer === true))
     });
-    if(filtered.length > 0) {
-      return true
+    if (filtered.length > 0) {
+      return true;
+    } else {
+      return false;
     }
   },
-  answeredNo:function(){
+  answeredNo: function() {
     var answersArray = this.answers;
     var currentUser = Meteor.userId();
-    var filtered = answersArray.filter(function(object){
+    var filtered = answersArray.filter(function(object) {
       return ((object.userId === currentUser) && (object.answer === false))
     });
-    if(filtered.length > 0) {
-      return true
-    }
-  },
-  votedYes:function(){
-    var answersArray = this.answers;
-    var currentUser = Meteor.userId();
-    var filtered = answersArray.filter(function(object){
-      return ((object.userId === currentUser) && (object.answer === true))
-    });
-    if(filtered.length == 1) {
-      return "disabled"
-    }
-  },
-  votedNo:function(){
-    var answersArray = this.answers;
-    var currentUser = Meteor.userId();
-    var filtered = answersArray.filter(function(object){
-      return ((object.userId === currentUser) && (object.answer === false))
-    });
-    if(filtered.length == 1) {
-      return "disabled"
+    if (filtered.length > 0) {
+      return true;
+    } else {
+      return false;
     }
   }
 });
 
 
 Template.votingSmall.helpers({
-  unanswered:function(){
+  id: function() {
+    return this._id;
+  },
+  unanswered: function() {
     var answersArray = this.answers;
     var currentUser = Meteor.userId();
-    var filtered = answersArray.filter(function(object){
+    var filtered = answersArray.filter(function(object) {
       return (object.userId === currentUser)
     });
-    if(filtered.length === 0) {
+    if (filtered.length === 0) {
       return true
     }
   },
-  answered:function(){
+  answered: function() {
     var answersArray = this.answers;
     var currentUser = Meteor.userId();
-    var filtered = answersArray.filter(function(object){
+    var filtered = answersArray.filter(function(object) {
       return (object.userId === currentUser)
     });
-    if(filtered.length > 0) {
+    if (filtered.length > 0) {
       return true
     }
   },
-  answeredYes:function(){
+  answeredYes: function() {
     var answersArray = this.answers;
     var currentUser = Meteor.userId();
-    var filtered = answersArray.filter(function(object){
+    var filtered = answersArray.filter(function(object) {
       return ((object.userId === currentUser) && (object.answer === true))
     });
-    if(filtered.length > 0) {
+    if (filtered.length > 0) {
       return true
     }
   },
-  answeredNo:function(){
+  answeredNo: function() {
     var answersArray = this.answers;
     var currentUser = Meteor.userId();
-    var filtered = answersArray.filter(function(object){
+    var filtered = answersArray.filter(function(object) {
       return ((object.userId === currentUser) && (object.answer === false))
     });
-    if(filtered.length > 0) {
+    if (filtered.length > 0) {
       return true
     }
   },
-  votedYes:function(){
+  votedYes: function() {
     var answersArray = this.answers;
     var currentUser = Meteor.userId();
-    var filtered = answersArray.filter(function(object){
+    var filtered = answersArray.filter(function(object) {
       return ((object.userId === currentUser) && (object.answer === true))
     });
-    if(filtered.length == 1) {
-      return "disabled grayed"
+    if (filtered.length == 1) {
+      return true;
+      //  return "disabled grayed"
     } else {
-      return "btn-success"
+      return false;
+      //return "btn-success"
     }
   },
-  votedNo:function(){
+  votedNo: function() {
     var answersArray = this.answers;
     var currentUser = Meteor.userId();
-    var filtered = answersArray.filter(function(object){
+    var filtered = answersArray.filter(function(object) {
       return ((object.userId === currentUser) && (object.answer === false))
     });
-    if(filtered.length == 1) {
-      return "disabled grayed"
+    if (filtered.length == 1) {
+      return true;
     } else {
-      return "btn-danger"
+      return false;
     }
   }
 });
 
-Template.viewQuestion.events({
-    'click .theme-link': function(event) {
-      event.preventDefault();
-      var theTheme = event.target.text;
-      Router.go('viewTheme',{theme:theTheme});
-    },
-    'click #newVoteYes': function(event) {
-      event.preventDefault();
-      var currentUser = Meteor.userId();
-      currentQuestion = this._id;
-      if(!currentUser){
-        alert('sorry, you must be logged in to do that. Please sign up or login now!')
-      } else {
-        Meteor.call('newVoteYes', currentQuestion);
-      }
-    },
-    'click #newVoteNo': function(event) {
-      event.preventDefault();
-      var currentUser = Meteor.userId();
-      currentQuestion = this._id;
-      if(!currentUser){
-        alert('sorry, you must be logged in to do that. Please sign up or login now!')
-      } else {
-        Meteor.call('newVoteNo', currentQuestion);
-      }
-    },
-    'click #changeVoteYes': function(event) {
-      event.preventDefault();
-      var currentUser = Meteor.userId();
-      currentQuestion = this._id;
-      if(!currentUser){
-        alert('sorry, you must be logged in to do that. Please sign up or login now!')
-      } else {
-        Meteor.call('changeVoteYes', currentQuestion);
-      }
-    },
-    'click #changeVoteNo': function(event) {
-      event.preventDefault();
-      var currentUser = Meteor.userId();
-      currentQuestion = this._id;
-      if(!currentUser){
-        alert('sorry, you must be logged in to do that. Please sign up or login now!')
-      } else {
-        Meteor.call('changeVoteNo', currentQuestion);
-      }
-    },
-    'click #revoteYes': function(event) {
-      event.preventDefault();
-      alert("you already voted Yes");
-    },
-    'click #revoteNo': function(event) {
-      event.preventDefault();
-      alert("you already voted No");
+Template.votingSmall.onRendered(function() {
+  $('.switch')['bootstrapSwitch']();
+  $('.switch').on('switchChange.bootstrapSwitch', function(event, state) {
+    var currentUser = Meteor.userId();
+    var currentQuestion = this.id;
+    // console.log('currentQuestion (OSC): '+currentQuestion);
+
+    if (!currentUser) {
+      alert('sorry, you must be logged in to do that. Please sign up or login now!')
+    } else {
+      Meteor.call('updateVote', state, currentQuestion);
     }
   });
+  this.autorun(function(c) {
+    var currentQuestion = Template.currentData()._id;
+    // console.log('currentQuestion (AR): '+currentQuestion);
+    var answersArray = Template.currentData().answers;
+    var currentUser = Meteor.userId();
+    var filtered = answersArray.filter(function(object) {
+      return (object.userId === currentUser)
+    });
+    if (filtered.length > 0) {
+      $('#' + currentQuestion).bootstrapSwitch('state', filtered[0].answer, filtered[0].answer);
+    }
+  });
+});
+
+Template.voting.onRendered(function() {
+  $('.switch')['bootstrapSwitch']();
+  $('.switch').on('switchChange.bootstrapSwitch', function(event, state) {
+    var currentUser = Meteor.userId();
+    var currentQuestion = this.id;
+    // console.log('currentQuestion (OSC): '+currentQuestion);
+    if (!currentUser) {
+      alert('sorry, you must be logged in to do that. Please sign up or login now!')
+    } else {
+      Meteor.call('updateVote', state, currentQuestion);
+    }
+  });
+  this.autorun(function(c) {
+    var currentQuestion = Template.currentData()._id;
+    // console.log('currentQuestion (AR): '+currentQuestion);
+    var answersArray = Template.currentData().answers;
+    var currentUser = Meteor.userId();
+    var filtered = answersArray.filter(function(object) {
+      return (object.userId === currentUser)
+    });
+    if (filtered.length > 0) {
+      $('#' + currentQuestion).bootstrapSwitch('state', filtered[0].answer, filtered[0].answer);
+    }
+  });
+});
+
+Template.viewQuestion.events({
+  'click .theme-link': function(event) {
+    event.preventDefault();
+    var theTheme = event.target.text;
+    Router.go('viewTheme', {
+      theme: theTheme
+    });
+  }
+});
