09e1b763fc8b037476343802f56f43f570eda5c3
glen0071 <glen0071@gmail.com>
2016-01-15 04:13:24 -0600

    display questions answered by user on userProfile page

--- client/interface/accounts/userProfile.js
+++ client/interface/accounts/userProfile.js
@@ -1,35 +1,49 @@
 // A user can view and edit his profile from here
-Template.userProfile.onCreated(function () {
-    this.subscribe('questions',{limit:100});
-    // limit to questions user created or answered
+Template.userProfile.onCreated(function() {
+  this.subscribe('questions', {
+    limit: 100
+  });
+  // limit to questions user created or answered
 });
 
 
 Template.userProfile.helpers({
-  questionsAsked: function(){
-    return Questions.find({createdBy: this._id}).count();
+  questionsAsked: function() {
+    return Questions.find({
+      createdBy: this._id
+    }).count();
   },
-  questionsUserAsked: function(){
-    return Questions.find({createdBy: this._id});
+  questionsUserAsked: function() {
+    return Questions.find({
+      createdBy: this._id
+    });
   },
-  questionsAnswered: function(){
-    return Questions.find({"answers.userId": this._id}).count();
+  questionsAnswered: function() {
+    return Questions.find({
+      "answers.userId": this._id
+    }).count();
   },
-  questionsUserAnswered: function(){
-    return Questions.find({"answers.userId": this._id});
+  questionsUserAnswered: function() {
+    return Questions.find({
+      "answers.userId": this._id
+    });
   },
-  userName:function(){
-    var user = Meteor.users.findOne({_id:this._id});
+  userName: function() {
+    var user = Meteor.users.findOne({
+      _id: this._id
+    });
     return user.username;
   },
-  points:function(){
-    return Meteor.users.findOne({_id:this._id}).points;
+  points: function() {
+    return Meteor.users.findOne({
+      _id: this._id
+    }).points;
   },
-  hideFollowBtn:function() {
+  hideFollowBtn: function() {
     var userViewed = this.username;
-    var array =  Meteor.user().profile.followingUsers;
+    var array = Meteor.user().profile.followingUsers;
     if ($.inArray(userViewed, array) >= 0) {
-        return "hideFollowBtn"
+      return "hideFollowBtn"
     } else {
       return ""
     }
@@ -37,37 +51,55 @@ Template.userProfile.helpers({
 });
 
 Template.userProfile.events({
-  'click .follow-user': function(event, template){
+  'click .follow-user': function(event, template) {
     event.preventDefault();
     var username = this.username;
     Meteor.call('followUser', username)
   },
-  'click .unfollow-user': function(event, template){
+  'click .unfollow-user': function(event, template) {
     event.preventDefault();
     var username = this.username;
     Meteor.call('unfollowUser', username)
   }
 });
 
-Template.registerHelper("determineEmail", function(){
-  var user = Meteor.users.findOne({_id: Meteor.userId()});
+Template.userAnswers.helpers({
+  aUserAnswer: function() {
+    var viewedUsersId = Template.parentData(1)._id;
+    var array = this.answers;
+    for (i in array) {
+      if (array[i].userId == viewedUsersId) {
+        if (array[i].answer == true) {
+          var hisAnswer = "yes";
+        } else {
+          var hisAnswer = "no";
+        }
+      }
+    }
+    return hisAnswer;
+  }
+});
+
+Template.registerHelper("determineEmail", function() {
+  var user = Meteor.users.findOne({
+    _id: Meteor.userId()
+  });
   var emailAddress, services;
   if (user.emails) {
     return emailAddress = user.emails[0].address;
   } else if (user.services) {
     services = user.services;
-      switch (false) {
-        case !services.facebook:
-          return services.facebook.email;
-        case !services.google:
-          return services.google.email;
-        case !services.twitter:
-          return null;
-        default:
-          return null;
-      }
+    switch (false) {
+      case !services.facebook:
+        return services.facebook.email;
+      case !services.google:
+        return services.google.email;
+      case !services.twitter:
+        return null;
+      default:
+        return null;
+    }
   } else {
     return null;
   }
-}
-);
+});
